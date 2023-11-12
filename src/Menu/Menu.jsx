e460ad3073f119f2c80a61b2bcc6a099f86fc06b
mukitur <mukit.inway@gmail.com>
2023-11-12 09:59:32 +0600

    cover component done

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -1,12 +1,30 @@
 import TitleHelmet from '../Components/TitleHelmet/TitleHelmet';
 import Cover from '../Pages/Shared/Cover/Cover';
+import bgimg from '../assets/menu/banner3.jpg';
+import PopularMenu from '../Pages/Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
   return (
     <div>
       <TitleHelmet title="Bistro Boss | Menu" />
-      <Cover />
-      <h2>Menu</h2>
+      <Cover
+        img={bgimg}
+        title="Our Menu"
+        shortdescription="Would you like to try a dish?"
+      />
+      <PopularMenu />
+      <Cover
+        img={bgimg}
+        title="Our Menu"
+        shortdescription="Would you like to try a dish?"
+      />
+      <PopularMenu />
+      <Cover
+        img={bgimg}
+        title="Our Menu"
+        shortdescription="Would you like to try a dish?"
+      />
+      <PopularMenu />
     </div>
   );
 };
--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -1,22 +1,16 @@
-const Cover = () => {
+const Cover = ({ title, img, shortdescription }) => {
   return (
     <div
-      className="hero h-[500px]"
+      className="hero h-[400px]"
       style={{
-        backgroundImage:
-          'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)',
+        backgroundImage: `url("${img}")`,
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+        <div className="max-w-xl bg-slate-300 px-40 bg-opacity-10 py-10">
+          <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+          <p className="mb-5">{shortdescription}</p>
         </div>
       </div>
     </div>
