c0763ce15189faa538ff37aeeecd4ac942d744eb
mukitur <mukit.inway@gmail.com>
2023-11-14 20:39:24 +0600

    captcha adden into form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,26 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import TitleHelmet from '../../Components/TitleHelmet/TitleHelmet';
 import Swal from 'sweetalert2';
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  validateCaptcha,
+} from 'react-simple-captcha';
 
 const Login = () => {
+  const [disabled, setDisabled] = useState(true);
   const { signInUser, signInWithGoogle } = useContext(AuthContext);
   const location = useLocation();
   const navigate = useNavigate();
+
+  const form = location.state?.from?.pathname || '/';
+  console.log('state in the location login page', form.state);
+
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -40,6 +53,16 @@ const Login = () => {
       .catch((error) => {
         console.log(error);
       });
+  };
+
+  // Captcha
+  const handleValidateCaptcha = (e) => {
+    const user_captcha_value = e.target.value;
+    if (validateCaptcha(user_captcha_value)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
   };
 
   return (
@@ -81,17 +104,41 @@ const Login = () => {
                 className="input input-bordered"
                 required
               />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
+            </div>
+            {/* captcha */}
+            <div className="form-control">
+              <label className="label border mb-3">
+                <LoadCanvasTemplate />
               </label>
+              <input
+                onBlur={handleValidateCaptcha}
+                type="text"
+                name="captcha"
+                placeholder="type the captcha above"
+                className="input input-bordered"
+              />
             </div>
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+            {/* catcha end */}
+
             <div className="form-control mt-6">
-              <button className="btn bg-[#3a77d9] hover:bg-[#628fd7] text-white  font-semibold">
+              <input
+                disabled={disabled}
+                className="btn bg-[#3a77d9] hover:bg-[#628fd7] text-white  font-semibold"
+                type="submit"
+                value="Login"
+              />
+              {/* <button
+                className="btn bg-[#3a77d9] hover:bg-[#628fd7] text-white  font-semibold"
+              >
                 Login
-              </button>
+              </button> */}
             </div>
+
             <div>
               <p className="font-poppins text-sm text-gray-600">
                 New to this site?
